1c9edb431dfbc0170e4bc21d100b5faca21c61a3
jamespatrickkk <orlanesjamespatrick@gmail.com>
2025-11-25 22:50:16 +0800

    modifying

--- json.js
+++ json.js
@@ -1,87 +1,87 @@
 var beaches = [
     {
         beach: "Anawangin Cove, Zambales",
-        img: "images/Anawangin.jpg",
+        img: "Images/Anawangin.jpg",
         desc: "Anawangin Beach in Zambales is known for its white sand, pine trees, and scenic views."
     },
     {
         beach: "Bagsit River, Zambales",
-        img: "images/Bagsit River.jpg",
+        img: "Images/Bagsit River.jpg",
         desc: "Bagsit River in Zambales is a spot for those who doesn't want to go to the beach but still want to experience the beauty of nature. It has clear waters and a peaceful environment."
     },
     {
         beach: "Big Lagoon, El Nido",
-        img: "images/Big Lagoon.jpg",
+        img: "Images/Big Lagoon.jpg",
         desc: "Big Lagoon in El Nido, Palawan features limestone cliffs and turquoise waters ideal for sightseeing."
     },
     {
         beach: "Matinloc Island, El Nido",
-        img: "images/Matinloc.jpg",
+        img: "Images/Matinloc.jpg",
         desc: "Matinloc Beach in Palawan is known for its stunning limestone formations and clear waters."
     },
     {
         beach: "Calatagan, Batangas",
-        img: "images/Calatagan.jpg",
+        img: "Images/Calatagan.jpg",
         desc: "Calatagan Beach in Batangas is known for its wide sandy shores, Starfish, and beautiful sunsets."
     },
     {
         beach: "Shimizu Island, El Nido",
-        img: "images/Shimizu.jpg",
+        img: "Images/Shimizu.jpg",
         desc: "Shimizu Island in El Nido is perfect for snorkeling and enjoying crystal-clear waters."
     },
     {
         beach: "Palad Sandbar, Marinduque",
-        img: "images/Marinduque.jpg",
+        img: "Images/Marinduque.jpg",
         desc: "Marinduque features peaceful beaches ideal for a relaxing getaway in the heart of the Philippines. it has a fine sandbar and clear waters."
     },
     {
         beach: "Coto Mines, Zambales",
-        img: "images/Coto Mines.jpg",
+        img: "Images/Coto Mines.jpg",
         desc: "Coto Mines Beach in Zambales is a hidden gem with clear waters and tranquil surroundings. This river is perfect for swimming and you can also do camping."
     },
     {
         beach: "Iba, Zambales",
-        img: "images/Iba.jpg",
+        img: "Images/Iba.jpg",
         desc: "Iba Beach in Zambales is perfect for beach walks and watching stunning sunsets. Zambales is also known for its delicious seafood, so be sure to try some local dishes while you're there."
     },
     {
         beach: "Laiya, Batangas",
-        img: "images/Laiya.jpg",
+        img: "Images/Laiya.jpg",
         desc: "Laiya Beach in Batangas offers white sand, calm waters, and resorts for a fun getaway."
     },
     {
         beach: "Laiya, Batangas",
-        img: "images/Laiya1.jpg",
+        img: "Images/Laiya1.jpg",
         desc: "Laiya Beach in Batangas is great for swimming, sunbathing, and enjoying scenic views. You can also try water sports like kayaking and banana boat rides."
     },
     {
         beach: "Lobo, Batangas",
-        img: "images/Lobo.jpg",
+        img: "Images/Lobo.jpg",
         desc: "Lobo Beach in Batangas is known for its quiet shores and relaxing environment."
     },
     {
         beach: "Cadlao Lagoon, El Nido",
-        img: "images/Cadlao.jpg",
+        img: "Images/Cadlao.jpg",
         desc: "Cadlao Lagoon in El Nido, Palawan is famous for its turquoise waters and dramatic limestone cliffs."
     },
     {
         beach: "Masasa, Tingloy, Batangas",
-        img: "images/Masasa.jpg",
+        img: "Images/Masasa.jpg",
         desc: "Masasa Beach in Batangas is a small, peaceful beach with white sand and clear water. It's perfect for if you want to relax, save money, and unwind."
     },
     {
         beach: "Mauban, Quezon",
-        img: "images/Mauban.jpg",
+        img: "Images/Mauban.jpg",
         desc: "Mauban Beach in Quezon offers serene surroundings and a quiet escape from the city."
     },
     {
         beach: "Sumbrero Island",
-        img: "images/Sumbrero Island.jpg",
+        img: "Images/Sumbrero Island.jpg",
         desc: "Sumbrero Island in Batangas is a tiny island shaped like a hat, perfect for island hopping. You can explore its clear waters, feeding fish, and enjoy snorkeling."
     },
     {
         beach: "Yambo Lake, Nagcarlan Laguna",
-        img: "images/Yambo.jpg",
+        img: "Images/Yambo.jpg",
         desc: "Yambo Lake in Nagcarlan Laguna is known for its green waters and relaxing coastal atmosphere."
     }
 ];
